2a60d13404a2a390f42769b91a647a663b0693a0
Desmond Mullen <dsm@busylittlestudios.com>
2019-03-22 19:19:12 -0400

    wororking

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -1,35 +1,53 @@
-// *********************************************************************************
-// api-routes.js - this file offers a set of routes for displaying and saving data to the db
-// *********************************************************************************
-
-// Dependencies
-// =============================================================
-
-// Grabbing our models
-
 const db = require("../models");
-
 // Routes
 // =============================================================
 module.exports = (app) => {
-
-  // GET route for getting all of the todos
-  app.get("/api/todos", (req, res) => {
-
+  // GET route for getting all of the burgers
+  app.get("/", (req, res) => {
+    db.Burger.findAll({}).then((result) => {
+      res.json(result);
+    });
   });
 
-  // POST route for saving a new todo. You can create a todo using the data on req.body
-  app.post("/api/todos", (req, res) => {
-
+  // POST route for saving a new burger. We can create a burger using the data on req.body
+  app.post("/", (req, res) => {
+    db.Burger.create({ text: req.body.text, complete: req.body.complete })
+      .then(() => db.Burger.findAll({}))
+      .then((result) => {
+        res.json(result);
+      })
+      .catch((err) => {
+        console.log(err.Error.msg);
+        res.json(err);
+      });
   });
 
-  // DELETE route for deleting todos. You can access the todo's id in req.params.id
-  app.delete("/api/todos/:id", (req, res) => {
-
+  // DELETE route for deleting burgers. We can access the ID of the burger to delete in
+  app.delete("/:id", (req, res) => {
+    db.Burger.destroy({
+      where: {
+        id: req.params.id
+      }
+    }).then(() => db.Burger.findAll({}))
+      .then((result) => {
+        res.json(result);
+      });
   });
 
-  // PUT route for updating todos. The updated todo will be available in req.body
-  app.put("/api/todos", (req, res) => {
-
+  // PUT route for updating burgers. We can access the updated burger in req.body
+  app.put("/", (req, res) => {
+    console.log(req.body.id, req.body.complete);
+    db.Burger.update({
+      id: req.body.id,
+      text: req.body.text,
+      complete: req.body.complete
+    }, {
+        where: {
+          id: req.body.id
+        }
+      }).then(() => db.Burger.findAll({}))
+      .then((result) => {
+        res.json(result);
+      });
   });
 };
