81cf2a6509940e5b229d7a5138dc92c6120737ba
Desmond Mullen <dsm@busylittlestudios.com>
2019-03-22 20:26:25 -0400

    wororking

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -2,52 +2,47 @@ const db = require("../models");
 // Routes
 // =============================================================
 module.exports = (app) => {
+  function displayAll(res) {
+    db.Burger.findAll({}).then((result) => {
+      const hbsObject = {
+        burgers: result
+      };
+      res.render('index', hbsObject);
+    });
+  };
+
   // GET route for getting all of the burgers
   app.get("/", (req, res) => {
-    db.Burger.findAll({}).then((result) => {
-      res.json(result);
-    });
+    displayAll(res);
   });
 
   // POST route for saving a new burger. We can create a burger using the data on req.body
   app.post("/", (req, res) => {
-    db.Burger.create({ text: req.body.text, complete: req.body.complete })
-      .then(() => db.Burger.findAll({}))
-      .then((result) => {
-        res.json(result);
-      })
-      .catch((err) => {
-        console.log(err.Error.msg);
-        res.json(err);
-      });
-  });
-
-  // DELETE route for deleting burgers. We can access the ID of the burger to delete in
-  app.delete("/:id", (req, res) => {
-    db.Burger.destroy({
-      where: {
-        id: req.params.id
-      }
-    }).then(() => db.Burger.findAll({}))
-      .then((result) => {
-        res.json(result);
-      });
+    if (req.body.burger_name) {
+      db.Burger.create({ burger_name: req.body.burger_name })
+        .then(() => {
+          displayAll(res);
+        })
+        .catch((err) => {
+          console.log(err.Error.msg);
+          res.json(err);
+        });
+    } else {
+      displayAll(res);
+    }
   });
 
   // PUT route for updating burgers. We can access the updated burger in req.body
-  app.put("/", (req, res) => {
-    console.log(req.body.id, req.body.complete);
+  app.put("/:id", (req, res) => {
     db.Burger.update({
-      id: req.body.id,
-      text: req.body.text,
-      complete: req.body.complete
+      devoured: true,
+      last_one_devoured: true
     }, {
         where: {
-          id: req.body.id
+          id: req.params.id
         }
-      }).then(() => db.Burger.findAll({}))
-      .then((result) => {
-        res.json(result);
+      }).then(() => {
+        displayAll(res);
       });
   });
 };
