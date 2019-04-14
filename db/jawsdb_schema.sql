5b3fed2216804b50c6f70e9d7abae78d44b9bf99
Desmond Mullen <dsm@busylittlestudios.com>
2019-04-13 21:23:35 -0400

    fifinal

--- db/jawsdb_schema.sql
+++ db/jawsdb_schema.sql
@@ -1,16 +1,60 @@
+drop database if exists km8fo0k3zvnlehjq;
+create database km8fo0k3zvnlehjq;
+
 use km8fo0k3zvnlehjq;
 
-create table burgers
+create table Customers
 (
     id integer not null
     auto_increment,
-burger_name varchar
+customer_name varchar
     (50) not null,
-devoured boolean not null default 0,
-last_one_devoured boolean
-not null default 0,
-createdAt TIMESTAMP default CURRENT_TIMESTAMP,
+    display_this_name boolean not null default 0,
+    createdAt TIMESTAMP
+default CURRENT_TIMESTAMP,
 updatedAt TIMESTAMP,
 primary key
     (id)
 );
+
+    create table Burgers
+    (
+        id integer not null
+        auto_increment,
+burger_name varchar
+        (50) not null,
+devoured boolean not null default 0,
+last_one_devoured boolean
+not null default 0,
+maker varchar
+        (50),
+eater varchar
+        (50),
+        createdAt TIMESTAMP
+default CURRENT_TIMESTAMP,
+updatedAt TIMESTAMP,
+primary key
+        (id)
+);
+
+        USE km8fo0k3zvnlehjq;
+
+        INSERT INTO Customers
+            (customer_name)
+        VALUES
+            ('Desmond'),
+            ('Vivian'),
+            ('Angie'),
+            ('Peter'),
+            ('Liam')
+        ;
+
+        INSERT INTO Burgers
+            (burger_name, maker)
+        VALUES
+            ('Yum Burger', 'Angie'),
+            ('Zippy Burger', 'Peter'),
+            ('Wow Burger', 'Desmond'),
+            ('Zoinks Burger', 'Liam'),
+            ('Unbeliev-O Burger', 'Desmond')
+        ;
